0a026e8a371c61b6fbc458bc8ddbf168f27a4883
diantouNO <diantounoo@163.com>
2016-11-03 01:16:16 -0700

    Add files via upload

--- ex4/MapperOfIPFilter.java
+++ ex4/MapperOfIPFilter.java
@@ -1,12 +1,53 @@
 package No4;
 
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MapperOfIPFilter {
-	public static void main(String[] args){
+	public static void main(String[] args) throws ParseException, FileNotFoundException {
+		Locale locale = Locale.US;
+		SimpleDateFormat regularFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", locale);
+		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		long beginDate = 0;
+		long endDate = 0;
+		//判断输入的长度是否大于1，至少需要两个时间
+		if (args.length > 1) {
+			beginDate = inputFormat.parse(args[0]).getTime();
+			endDate = inputFormat.parse(args[1]).getTime();
+		}
 		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
+		String patternIP = "(\\d+.\\d+.\\d+.\\d+)"; // 匹配IP
+		String patternIPandTime = "(\\[.*\\])"; // 匹配IP和时间
+		Pattern rIP = Pattern.compile(patternIP);
+		Pattern rIPandTime = Pattern.compile(patternIPandTime);
+
+		while (scanner.hasNext()) {
+			// 对每行进行处理
 			String line = scanner.nextLine();
+			// 切分获取IP，Time
+			String strIp = null;
+			String strTime = null;
+			long lineTimestamp = 0;
+			Date lineDate = null;
+			Matcher mIP = rIP.matcher(line);
+			if (mIP.find()) {
+				strIp = mIP.group(1);
+			}
+			Matcher mIPandTime = rIPandTime.matcher(line);
+			if (mIPandTime.find()) {
+				strTime = mIPandTime.group(1).substring(1, 21);
+				lineDate = regularFormat.parse(strTime);
+				lineTimestamp = lineDate.getTime();
+			}
+			// 对在时间区间内的数据进行输出，将满足时间条件的IP输出
+			if (lineTimestamp >= beginDate && lineTimestamp <= endDate)
+				System.out.println(strIp);
 		}
 	}
 }
--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,24 @@
 package No4;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
+/*
+ * 运行命令
+ * cat a | java -jar Mapper.jar "2015-12-31 18:00:00" "2015-12-31 19:00:00" | java -jar Reducer.jar
+ */
 public class ReducerOfIPFilter {
 	public static void main(String[] args){
+		//接受Map输出的IP
 		Scanner scanner = new Scanner(System.in);
+		//用Map来存储输出的IP
+		Map map = new HashMap();
 		while (scanner.hasNext()){
 			String line = scanner.nextLine();
+			//将IP作为Key存储，去重
+			map.put(line, 1);
 		}
+		//统计map中键值对的数量，即为满足条件的IP数量
+		System.out.println(map.size());
 	}
 }
